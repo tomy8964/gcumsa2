28552e530042e9092bc7d482459dc0865bf13f7f
ham <tomy8964@naver.com>
2023-05-23 17:35:07 +0900

    exercise finish

---
+++ InHello/src/main/java/gcumsa/external/HelloServiceImpl.java
@@ -0,0 +1,9 @@
+package gcumsa.external;
+
+public class HelloServiceImpl implements HelloService {
+
+    @Override
+    public void hello(Hello hello) {
+        throw new UnsupportedOperationException("Unimplemented method 'hello'");
+    }
+}
